55b3987bf91c4e4f42c1f27cfb789116527b9a3e
gaitano <dev.gaitano@gmail.com>
2026-04-06 20:50:19 +0300

    feat: added and styled mock data to playbook summary

--- frontend/src/components/BrandPlaybook/BrandPlaybook.tsx
+++ frontend/src/components/BrandPlaybook/BrandPlaybook.tsx
@@ -14,7 +14,33 @@ function BrandPlaybook() {
             <p>o</p>
             <p>o</p>
           </div>
-          <div className="pb-summary"></div>
+          <div className="pb-summary">
+            <div>
+              <h1>KNöTRA</h1>
+              <h2>FASHION AND APPARELL</h2>
+            </div>
+            <div>
+              <div>
+                <p className="category-content">Knötra is a premium handmade clothing brand established in 2024, crafting high-quality knitwear and textile pieces with an artisanal touch. Rooted in the beauty of slow fashion, Knötra blends timeless design with meticulous craftsmanship. Offering warmly textured, thoughtfully made garments for those who value authenticity and enduring style.</p>
+              </div>
+              <div className="categories">
+                <div className="category">
+                  <p className="category-title">Tone</p>
+                  <p className="category-content">Calm and intentional</p>
+                </div>
+                <div className="category">
+                  <p className="category-title">Color palette</p>
+                  <div>
+                    <div className="pantone" id="color1"></div>
+                    <div className="pantone" id="color2"></div>
+                    <div className="pantone" id="color3"></div>
+                    <div className="pantone" id="color4"></div>
+                    <div className="pantone" id="color5"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="pb-saved-content"></div>
         <div className="pb-upload-container">
